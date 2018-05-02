f8f603ab1ed9f83a0320cbac8b7772e7870a755c
Muffin <marvin.dick96@web.de>
2018-05-02 19:08:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <footer class="container-fluid bg-4 text-center" id="myFooter">
+                <p>Example Footer made by Marvin Dick</p>
+            </footer>
+        );
+  }
+}
+
+export default App;
