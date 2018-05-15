a05eafd552566c1255bc0a144a1d6ac1bc9ec6fe
Muffin <marvin.dick96@web.de>
2018-05-15 19:15:30 +0200

    removed config change

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -133,14 +133,6 @@ module.exports = {
         // match the requirements. When no loader matches it will fall
         // back to the "file" loader at the end of the loader list.
         oneOf: [
-            {
-                test: /\.js$/,
-                loader: 'safe-externals-loader',
-                query: JSON.stringify({
-                    react: ['React'],
-                    'react-dom': ['ReactDOM']
-                })
-            },
           // "url" loader works just like "file" loader but it also embeds
           // assets smaller than specified size as data URLs to avoid requests.
           {
